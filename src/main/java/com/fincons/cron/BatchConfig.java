17552984a600cd7fa718429b281135fa80c05f38
g.salvio <giuseppe.salvio@finconsgroup.com>
2022-02-02 10:00:33 +0100

    rename vari e file csv

--- src/main/java/com/fincons/cron/BatchConfig.java
+++ src/main/java/com/fincons/cron/BatchConfig.java
@@ -14,39 +14,41 @@ import org.springframework.context.annotation.Configuration;
 @EnableBatchProcessing
 public class BatchConfig {
 
-    private final JobBuilderFactory jobs;
+    private final JobBuilderFactory jobBuilderFactory;
 
-    private StepBuilderFactory steps;
+    private StepBuilderFactory stepBuilderFactory;
 
-    public BatchConfig(JobBuilderFactory jobs, StepBuilderFactory steps) {
-        this.jobs = jobs;
-        this.steps = steps;
+    public BatchConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory steps) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = steps;
     }
 
     @Bean
     public Step stepOne(){
-        return steps.get("stepOne")
+        return stepBuilderFactory
+                .get("stepOne")
                 .tasklet(new MyTaskOne())
                 .build();
     }
 
     @Bean
     public Step stepTwo(){
-        return steps.get("stepTwo")
+        return stepBuilderFactory.get("stepTwo")
                 .tasklet(new MyTaskTwo())
                 .build();
     }
 
     @Bean
     public Step stepThree(){
-        return steps.get("stepTwo")
+        return stepBuilderFactory.get("stepTwo")
                 .tasklet(new MyTaskThree())
                 .build();
     }
 
     @Bean
     public Job demoJob(){
-        return jobs.get("demoJob")
+        return jobBuilderFactory
+                .get("demoJob")
                 .incrementer(new RunIdIncrementer())
                 .start(stepOne())
                 .next(stepTwo())
